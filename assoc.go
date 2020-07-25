e789ef69040abdca7071326b07ef940481c8edf1
MinWooJin <alsdn653@jam2in.com>
2020-07-26 00:45:05 +0900

    FEATURE: add some assoc module interface

--- assoc.go
+++ assoc.go
@@ -27,6 +27,18 @@ func assocInsert(hvalue uint32, it *item) bool {
 	return true
 }
 
+func assocDelete(hvalue uint32, key string) bool {
+	return true
+}
+
+func checkExpandTable() bool {
+	return false
+}
+
+func expandTable() bool {
+	return true
+}
+
 func initializeAssoc(hashtableSize uint64) {
 	assoc.hashtableSize = hashtableSize
 	assoc.hashtable = make([]*item, assoc.hashtableSize)
