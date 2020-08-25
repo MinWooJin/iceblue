2d14b6fdbab8e63d0abbc5f5cf5fabb6c5372693
minwoojin <alsdn653@gmail.com>
2020-08-25 20:19:54 +0900

    FEATURE: add process and send network request function

--- network.go
+++ network.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"strings"
 )
 
 type network struct {
@@ -11,6 +12,62 @@ type network struct {
 
 var networkInfo network
 
+func sendNetworkRequest(conn net.Conn, buffer []byte, size int) int {
+	length, err := conn.Write(buffer)
+	if err != nil {
+		return -1
+	}
+	if length != size {
+		return -1
+	}
+	return 0
+}
+
+/* Request format:
+ * <command> <key> [<valueLength>]\r\n[<value>\r\n]
+ */
+func processNetworkRequest(conn net.Conn) {
+	addr := conn.RemoteAddr().String()
+	log.Printf("Read network request. Conn = %s\n", addr)
+
+	buffer := make([]byte, 4096)
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			log.Printf("Network Read failure.\n")
+			goto close
+		}
+		if length > 0 {
+			data := string(buffer[:length])
+			position := strings.Index(data, "\\r\\n")
+
+			if position < 0 {
+				/* TODO :: change error write after define error code */
+				response := "INVALID command foramt"
+				_, err = conn.Write([]byte(response))
+				if err != nil {
+					log.Printf("Network Write failure.\n")
+					goto close
+				}
+			} else {
+				/* process command */
+				ret := processCommand(data, position)
+				if ret < 0 {
+					log.Printf("Failure process command.\n")
+					goto close
+				}
+			}
+		} else {
+			log.Printf("Network Read length is zero.\n")
+			goto close
+		}
+	}
+
+close:
+	log.Printf("Close connection. Client Addr = %s\n", addr)
+	conn.Close()
+}
+
 func acceptNetworkProcess() {
 	log.Printf("Start accept network process\n")
 	for networkInfo.listener != nil {
@@ -18,10 +75,7 @@ func acceptNetworkProcess() {
 		if err != nil {
 			log.Panic(err)
 		}
-		/* DEBUG code */
-		log.Printf("asscepted")
-		conn.Close()
-		break
+		go processNetworkRequest(conn)
 	}
 	log.Printf("Stop accept network process\n")
 }
--- process.go
+++ process.go
@@ -4,6 +4,13 @@ import (
 	"log"
 )
 
+func processCommand(data string, position int) int {
+	/* DEBUG code */
+	log.Printf("process command. data = %s, position %d\n", data, position)
+
+	return 0
+}
+
 func initializeProcessRoutine(routineCount int) {
 	log.Printf("Initialize process routine\n")
 }
