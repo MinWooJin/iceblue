489b2d374051d7e147800132ddf4cb40f4c74e40
minwoojin <alsdn653@gmail.com>
2020-10-17 13:15:56 +0900

    CLEANUP: add operation code for readability

--- process.go
+++ process.go
@@ -20,6 +20,7 @@ const (
 	DELETE
 	GET
 	STATS
+	QUIT
 	UNKNOWN = -1
 )
 
@@ -82,112 +83,127 @@ func getOperationCode(operationToken string, tokenCount int) int {
 		return DELETE
 	} else if operationToken == "get" && tokenCount == 2 {
 		return GET
-	} else if operationToken == "stats" && tokenCount == 2 {
+	} else if operationToken == "stats" && tokenCount == 1 {
 		return STATS
 	} else {
 		return UNKNOWN
 	}
 }
 
-func processCommand(conn net.Conn, data string, position int, endPosition int) int {
-	tokenCount, tokens := tokenizeCommand(data, position)
-
-	/* TODO :: define operation code using operationToken and refactoring to switch case */
-	/*
-		operationCode := getOperationCode(tokens[operationToken], tokenCount)
-		switch operationCode {
-		case SET, UPDATE:
-			return processStoreCommand()
-		case DELETE:
-			return processDeleteCommand()
-		case GET:
-			return processGetCommand()
-		case STATS:
-			return processStatsCommand()
-		case UNKNOWN:
-			return processUnknownCommand()
-		}
-	*/
-
-	if (tokens[operationToken] == "set" || tokens[operationToken] == "update") && tokenCount == 3 {
-		/* Input foramt : set {key} {vlen}\r\n{value}\r\n */
-		/* TODO :: check if limits are needed of key, value length */
-		for {
-			key := tokens[keyToken]
-			vlen, err := strconv.Atoi(tokens[keyToken+1])
-			if err != nil {
-				if sendNetworkRequest(conn, "CLIENT_ERROR bad line format") < 0 {
-					return -1
-				}
-				break
-			}
-			value, ret := tryReadNextline(conn, data, endPosition, vlen)
-			if ret < 0 {
-				if sendNetworkRequest(conn, "CLIENT_ERROR failed value read") < 0 {
-					return -1
-				}
-				break
-			}
-			/* TODO :: define operation code and use that */
-			if tokens[operationToken] == "set" {
-				ret = store(key, value)
-			} else {
-				ret = update(key, value)
-			}
-			if ret == 0 {
-				if sendNetworkRequest(conn, "SUCCESS") < 0 {
-					return -1
-				}
-			} else {
-				/* TODO :: error hanlding according to error code */
-				if sendNetworkRequest(conn, "SERVER_ERROR store failed") < 0 {
-					return -1
-				}
-			}
-			break
-		}
-	} else if tokens[operationToken] == "delete" && tokenCount == 2 {
-		for {
-			key := tokens[keyToken]
-			delete(key)
-			if sendNetworkRequest(conn, "DELETED") < 0 {
-				return -1
-			}
-			break
-		}
-	} else if tokens[operationToken] == "get" && tokenCount == 2 {
-		for {
-			var buffer bytes.Buffer
-			key := tokens[keyToken]
-			value, ret := get(key)
-			if ret == 0 {
-				/* separate header and body to support multiple keys */
-				headerStr := fmt.Sprintf("VALUE %d\r\n", len(value))
-				bodyStr := fmt.Sprintf("%s\r\n", value)
-				buffer.WriteString(headerStr)
-				buffer.WriteString(bodyStr)
-			} else {
-				/* TODO :: error hanlding according to error code */
-			}
-			buffer.WriteString("END\r\n")
-			if sendNetworkRequest(conn, buffer.String()) < 0 {
-				return -1
-			}
-			break
-		}
-	} else if tokens[operationToken] == "stats" {
-		for {
-			break
-		}
-	} else {
-		log.Printf("Unknown operation = %s, addr = %s\n",
-			tokens[operationToken], conn.RemoteAddr().String())
-
-		if sendNetworkRequest(conn, "CLIENT_ERROR unknown operation") < 0 {
+func processStoreCommand(conn net.Conn, key string, velnStr string, data string, endPosition int) int {
+	vlen, err := strconv.Atoi(velnStr)
+	if err != nil {
+		if sendNetworkRequest(conn, "CLIENT_ERROR bad line format") < 0 {
 			return -1
 		}
 	}
+	value, ret := tryReadNextline(conn, data, endPosition, vlen)
+	if ret < 0 {
+		if sendNetworkRequest(conn, "CLIENT_ERROR failed value read") < 0 {
+			return -1
+		}
+	}
+	ret = store(key, value)
+	if ret == 0 {
+		if sendNetworkRequest(conn, "SUCCESS") < 0 {
+			return -1
+		}
+	} else {
+		/* TODO :: error hanlding according to error code */
+		if sendNetworkRequest(conn, "SERVER_ERROR store failed") < 0 {
+			return -1
+		}
+	}
+	return 0
+}
 
+func processUpdateCommand(conn net.Conn, key string, velnStr string, data string, endPosition int) int {
+	vlen, err := strconv.Atoi(velnStr)
+	if err != nil {
+		if sendNetworkRequest(conn, "CLIENT_ERROR bad line format") < 0 {
+			return -1
+		}
+	}
+	value, ret := tryReadNextline(conn, data, endPosition, vlen)
+	if ret < 0 {
+		if sendNetworkRequest(conn, "CLIENT_ERROR failed value read") < 0 {
+			return -1
+		}
+	}
+	ret = update(key, value)
+	if ret == 0 {
+		if sendNetworkRequest(conn, "SUCCESS") < 0 {
+			return -1
+		}
+	} else {
+		/* TODO :: error hanlding according to error code */
+		if sendNetworkRequest(conn, "SERVER_ERROR update failed") < 0 {
+			return -1
+		}
+	}
+	return 0
+}
+
+func processDeleteCommand(conn net.Conn, key string) int {
+	delete(key)
+	if sendNetworkRequest(conn, "DELETED") < 0 {
+		return -1
+	}
+	return 0
+}
+
+func processGetCommand(conn net.Conn, key string) int {
+	var buffer bytes.Buffer
+	value, ret := get(key)
+	if ret == 0 {
+		/* separate header and body to support multiple keys */
+		headerStr := fmt.Sprintf("VALUE %d\r\n", len(value))
+		bodyStr := fmt.Sprintf("%s\r\n", value)
+		buffer.WriteString(headerStr)
+		buffer.WriteString(bodyStr)
+	} else {
+		/* TODO :: error hanlding according to error code */
+	}
+	buffer.WriteString("END\r\n")
+	if sendNetworkRequest(conn, buffer.String()) < 0 {
+		return -1
+	}
+	return 0
+}
+
+func processStatsCommand(conn net.Conn) int {
+	/* TODO :: process stats command */
+	return 0
+}
+
+func processUnknownCommand(conn net.Conn, operation string) int {
+	log.Printf("Unknown operation = %s, addr = %s\n",
+		operation, conn.RemoteAddr().String())
+
+	if sendNetworkRequest(conn, "CLIENT_ERROR unknown operation") < 0 {
+		return -1
+	}
+	return 0
+}
+
+func processCommand(conn net.Conn, data string, position int, endPosition int) int {
+	tokenCount, tokens := tokenizeCommand(data, position)
+
+	operationCode := getOperationCode(tokens[operationToken], tokenCount)
+	switch operationCode {
+	case SET:
+		return processStoreCommand(conn, tokens[keyToken], tokens[keyToken+1], data, endPosition)
+	case UPDATE:
+		return processUpdateCommand(conn, tokens[keyToken], tokens[keyToken+1], data, endPosition)
+	case DELETE:
+		return processDeleteCommand(conn, tokens[keyToken])
+	case GET:
+		return processGetCommand(conn, tokens[keyToken])
+	case STATS:
+		return processStatsCommand(conn)
+	case UNKNOWN:
+		return processUnknownCommand(conn, tokens[operationToken])
+	}
 	return 0
 }
 
